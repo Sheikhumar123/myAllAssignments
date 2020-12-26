3cea8c19757924e333a4ee98d8af203275f49686
Sheikhumar123 <xhk.umar5091@gmail.com>
2020-12-26 21:23:52 +0500

    New edited

--- Program04/app.js
+++ Program04/app.js
@@ -7,20 +7,19 @@ if (number === 1) {
 else if (number === 2) {
     alert(number + " is a prime number");
 }
-else if(typeof number == "number"){
+else if (typeof number == "number") {
     for (var i = 2; i < number; i++) {
         if (number % i === 0) {
             alert(number + " is not a prime number");
             break;
+        }else{
+            alert(number + " Is a prime number")
+            break;
         }
-    }else{
-
-        alert(number + " is a prime number");
-
     }
-
-       
+}else{
+    alert("is a prime number")
+}      
         
-    }
 
 
