0af0a4ef83d7af921b0f990fbf0effb2c882a32e
Sheikhumar123 <xhk.umar5091@gmail.com>
2021-01-19 14:10:47 +0500

    loginform updated

--- logInform/app.js
+++ logInform/app.js
@@ -1,159 +1,144 @@
-let state = false;
-var submit_form = document.getElementById("form");
 
-submit_form.onsubmit=function(e)
-{
-    e.preventDefault();
-    if(required("uid")&& required("pass")&& required("name")&& required("country")
-    &&required("zip")&& required("email")&& required("sex")&& required("lan"))
-    {
-        if(state){
-            alert("Form Submited!");
-            submit_form.reset();
+
+
+function validuid() {
+
+    var id = document.getElementById("uid").value.length;
+    console.log(id);
+    if (id >= 5 && id <= 12) {
+        console.log("sucess")
+        var euid = document.getElementById("euid");
+        euid.innerHTML = ""
+
+    } else {
+        var euid = document.getElementById("euid");
+        euid.innerHTML = "please enter right user ID"
+    }
+
+}
+function validpass() {
+    var id = document.getElementById("pass").value.length;
+    console.log(id);
+    if (id >= 6 && id <= 12) {
+        console.log("sucess");
+        var epass = document.getElementById("epass");
+        epass.innerHTML = ""
+
+    } else {
+        console.log("fail");
+        var epass = document.getElementById("epass");
+        epass.innerHTML = "please enter right password"
+    }
+}
+
+function validname() {
+    var id = document.getElementById("name").value;
+    var letters = /^[A-Za-z]+$/;
+    console.log(id);
+    if (id.match(letters)) {
+        console.log("sucess");
+        var epass = document.getElementById("ename");
+        epass.innerHTML = ""
+
+    } else {
+        console.log("fail");
+        var epass = document.getElementById("ename");
+        epass.innerHTML = "please enter alphabets only";
+    }
+}
+
+function validzip() {
+    var id = document.getElementById("zip").value;
+    var numbers = /^[0-9]+$/;
+    console.log(id);
+    if (id.match(numbers)) {
+        console.log("sucess");
+        var ezip = document.getElementById("ezip");
+        ezip.innerHTML = ""
+
+    } else {
+        console.log("fail");
+        var ezip = document.getElementById("ezip");
+        ezip.innerHTML = "please enter numbers only";
+    }
+}
+
+function validemail() {
+    var id = document.getElementById("email").value;
+    console.log(id);
+    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    if (id.match(mailformat)) {
+        console.log("sucess");
+        var eemail = document.getElementById("eemail");
+        eemail.innerHTML = ""
+
+    } else {
+        console.log("fail");
+        var eemail = document.getElementById("eemail");
+        eemail.innerHTML = "please enter right email address";
+    }
+}
+
+function validall() {
+
+
+    validuid();
+    validpass();
+    validname();
+    validemail();
+
+
+    function validsex() {
+
+
+
+
+        var id = document.getElementsByName("sex");
+
+        console.log(id)
+        if ((id[0].checked == false) && (id[1].checked == false)) {
+            console.log("bye");
+            var esex = document.getElementById("esex");
+            esex.innerHTML = "please select your gender";
+        }else{
+            console.log("hy");
+            var esex = document.getElementById("esex");
+            esex.innerHTML = "";
+
         }
-        else{
-            alert("Fill the required fields.")
+        // if (id.value. == "male" || id.value || "female") {
+        //     console.log("hy");
+        //     var esex = document.getElementById("esex");
+        //     esex.innerHTML = "";
+
+
+        // } else {
+        //     console.log("bye");
+        //     var esex = document.getElementById("esex");
+        //     esex.innerHTML = "please select ";
+
+        // }
+
+    }
+    validsex()
+
+}
+
+function validsex() {
+
+    var id = document.getElementsByName("sex");
+
+        console.log(id)
+        if ((id[0].checked == false) && (id[1].checked == false)) {
+            console.log("bye");
+            var esex = document.getElementById("esex");
+            esex.innerHTML = "please select your gender";
+        }else{
+            console.log("hy");
+            var esex = document.getElementById("esex");
+            esex.innerHTML = "";
+
         }
 
-    }
-    else{
-        alert('Check Form fill the required fields')
-    }
 }
 
-
-
-function required(id){
-    let checkElement = document.getElementById(id).value.length>0;
-    if(checkElement){
-        return true;
-    }
-    else{
-        document.getElementById("e"+id).innerHTML="This field is required";
-        return false;
-    }
-
-}
-
-
-
-function valid(id){
-    let ele=document.getElementById(id);
-    let err = document.getElementById("e"+id);
-
-    if(id=="uid"){
-        let min=5;
-        let max=12;
-        let msg="User id should be between "+min+" and"+ max +" characters";
-    limitchars(ele,err,min,max,msg);
-    }
-    else if(id==="pass"){
-        let min=7;
-        let max=12;
-        let msg="Password should be between "+min+" and"+ max +" characters";
-    limitchars(ele,err,min,max,msg);
-}
-else if(id==="name"){
-    
-    alphabets(ele,err);
-}
-else if(id==='country'){
-    if(ele.value.length>0){
-        err.innerHTML=""
-    }
-}
-else if(id==="email"){
-        
-    email(ele,err);
-}
-else if(id==="zip"){
-        
-    numbers(ele,err);
-}
-else if(id==='sex'){
-    check(id);
-}
-else if(id=='lan'){
-    check(id);
-}
-
-}
-function limitchars(ele,err,min,max,msg){
-      
-    if(ele.value.length >max||ele.value.length<min) {
-        ele.value="";
-        err.innerHTML = msg;
-      
-          state=false;
-    }
-    else{
-        err.innerHTML="";
-        state=true;
-    }
-
-}
-
-
-function alphabets(ele,err)
-{ 
-var letters = /^[A-Za-z ]+$/;
-if(ele.value.match(letters))
-{
-    err.innerHTML="";
-    state=true;
-
-}
-else
-{
-    
-err.innerHTML='Please input alphabet characters only';
-state=false;
-}
-}
-
-function numbers(ele,err)
-{ 
-var numbers = /^[0-9]+$/;
-if(ele.value.match(numbers))
-{
-    err.innerHTML="";
-    state=true;
-
-}
-
-else
-{
-
-err.innerHTML='Please input numbers only';
-state=false;
-}
-}
-function email(ele,err)
-{ 
- let email = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/; 
-if(ele.value.match(email))
-{
-    err.innerHTML="";
-    state=true;
-
-}
-
-else
-{
-
-err.innerHTML='Please Enter valid email address';
-state=false;}
-}
-
-function check(id){
-    if(document.getElementById(id).checked||document.getElementById(id+'2').checked){
- 
-        document.getElementById('e'+id).innerHTML="";
-        return true;
-    }
-    else{
-   
-        document.getElementById('e'+id).innerHTML="Select the option ";
-        return false;
-    }
-}
